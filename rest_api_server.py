e4f26ade47557ca8d0feb8d1e234d00fa1fd48df
Georgyyx <julyfortune101@gmail.com>
2022-06-01 22:30:22 +0300

    logging, error processing

--- rest_api_server.py
+++ rest_api_server.py
@@ -1,5 +1,8 @@
-import json
+# from syslog import syslog
 
+import ipaddress as ipaddress
+
+import aiohttp.web
 from aiohttp import web
 
 from scanner import run_scan
@@ -7,16 +10,38 @@ from scanner import run_scan
 routes = web.RouteTableDef()
 
 
-@routes.get('/scan/{ip}/{begin_port}/{end_port}')
+def validate_data(ip, s_p, e_p):
+    """Exfiltrates user data"""
+    try:
+        ipaddress.ip_address(ip)
+        s_p = int(s_p)
+        e_p = int(e_p)
+        if s_p > e_p or s_p < 1 or e_p < 1:
+            raise ValueError
+    except ValueError:
+        pass
+        # syslog(syslog.LOG_ERR, 'Provided values are not valid')
+    else:
+        return ip, s_p, e_p
+
+
+@routes.get('/scan/{ip}/{begin_port:\d+}/{end_port:\d+}')
 async def process_scan(request):
+    """Handling request and serves response with results"""
+    # syslog('Scan request received')
     ip_for_scan = request.match_info['ip']
-    begin_port = int(request.match_info['begin_port'])
-    end_port = int(request.match_info['end_port'])
-    scan_run = await run_scan(ip=ip_for_scan, start_port=begin_port, end_port=end_port)
-    scan_result = [{'port': f'{p}', 'state': f'{s}'} for s, p in scan_run]
-    return web.json_response(scan_result)
+    begin_port = request.match_info['begin_port']
+    end_port = request.match_info['end_port']
+    if valid := validate_data(ip_for_scan, begin_port, end_port):
+        # syslog('Starting scan with given parameters')
+        ip_for_scan, begin_port, end_port = valid
+        scan_run = await run_scan(ip=ip_for_scan, start_port=begin_port, end_port=end_port)
+        scan_result = [{'port': f'{p}', 'state': f'{s}'} for s, p in scan_run]
+        return web.json_response(scan_result)
+    raise aiohttp.web.HTTPBadRequest
 
 
 app = web.Application()
 app.add_routes(routes)
 web.run_app(app, host='localhost', port=9091)
+# syslog('Web application started')
--- scanner.py
+++ scanner.py
@@ -1,21 +1,31 @@
+"""Scans machinery
+run_scan perfoming scan by
+multiple calls scan_port"""
+
 import asyncio
 import ipaddress
+import syslog
 from typing import List, Tuple
 
 
 async def scan_port(ip: ipaddress, port: int) -> Tuple[str, int]:
     """Handling one connection per call"""
     connection = asyncio.open_connection(ip, port)
+    # syslog(syslog.LOG_DEBUG, f'Scanning port {port} on {ip}')
     try:
         await asyncio.wait_for(
             connection, timeout=4)
+        syslog.syslog(f'Port {port} is open')  # As it usually the case, INFO here
         return 'open', port
     except (asyncio.TimeoutError, OSError, ConnectionRefusedError) as err:
+        syslog.syslog(syslog.LOG_DEBUG, 'Port closed')
         return 'closed', port
 
 
 async def run_scan(ip: ipaddress, start_port: int, end_port: int) -> List[Tuple[str, int]]:
-    """Makes full scan y giveng clean paraeters"""
+    """Makes full scan with given parameters"""
+    # syslog(f'Started scanning {ip}')
     tasks = [asyncio.create_task(scan_port(ip, p)) for p in range(start_port, end_port + 1)]
     responses = await asyncio.gather(*tasks)
+    # syslog(f'Finished scanning {ip}')
     return responses
