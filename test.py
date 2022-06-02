9341bcd176fc7963160926e75b0bfd68358380b2
Georgyyx <julyfortune101@gmail.com>
2022-06-02 20:38:41 +0300

    test server used as open port

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from aiohttp import web
 from aiohttp.abc import Application
 from aiohttp.test_utils import AioHTTPTestCase
 
-from sswa.rest_api_handlers import runable_app, process_scan
+from sswa.rest_api_handlers import process_scan
 
 
 class ScannerPointTestCase(AioHTTPTestCase):
@@ -14,10 +14,14 @@ class ScannerPointTestCase(AioHTTPTestCase):
         return app
 
     async def test_scan_res(self):
-        async with self.client.request('GET', '/scan/127.0.0.1/5430/5435') as resp:  # dummy host
+        test_server_port = self.server.port
+        async with self.client.request(
+                'GET', f'/scan/127.0.0.1/{test_server_port}/'
+                       f'{test_server_port}') as resp:
             self.assertEqual(resp.status, 200)
             answer = await resp.json()
-        self.assertIn({'port': '5433', 'state': 'open'}, answer, )
+        print(self.server.port)
+        self.assertIn({'port': f'{test_server_port}', 'state': 'open'}, answer, )
 
     async def test_scan_fail(self):
         async with self.client.request('GET', '/scan/127.0.0.1/65555/65666') as response:
