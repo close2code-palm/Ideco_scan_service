37269f149371b0230e3dfbe0afbeb4e99d9ce712
Georgyyx <julyfortune101@gmail.com>
2022-06-03 15:36:24 +0300

    packaging entry points and python version fix

--- sswa/run_server.py
+++ sswa/run_server.py
@@ -7,5 +7,8 @@ from rest_api_handlers import runable_app
 
 PORT = 9091
 
-syslog.syslog(f'Web application is starting on port {PORT}')
-web.run_app(runable_app, port=PORT)
+def main():
+    """Starts the app, via entry_point script"""
+    syslog.syslog(f'Web application is starting on port {PORT}')
+    web.run_app(runable_app, port=PORT)
+    
