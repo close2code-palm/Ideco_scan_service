05505b205c84472836d8bdaf5b3d982390878d95
Georgyyx <julyfortune101@gmail.com>
2022-06-03 15:32:21 +0300

    callable server for entry_oint

--- sswa/run_server.py
+++ sswa/run_server.py
@@ -5,5 +5,7 @@ try:
 except ModuleNotFoundError:
     from sswa.rest_api_handlers import runable_app
 
+
 # syslog.syslog('Web application is starting')
-web.run_app(runable_app, host='localhost', port=9091)
+def main():
+    web.run_app(runable_app, host='localhost', port=9091)
